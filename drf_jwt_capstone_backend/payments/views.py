634ac180e0abf6a09c469b559b68aaadbba1980c
andrew morales <andrew.morales725@gmail.com>
2021-12-31 14:21:19 -0500

    Created checkout view

--- drf_jwt_capstone_backend/payments/views.py
+++ drf_jwt_capstone_backend/payments/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django.conf import settings
+from rest_framework.response import Response
+import stripe
+from drf_jwt_capstone_backend.local_settings import *
+from rest_framework.views import APIView
+from rest_framework import status
+from django.shortcuts import redirect
 
-# Create your views here.
+stripe.api_key = STRIPE_SECRET_KEY
+
+class StripeCheckoutView(APIView):
+    def post(self, request, customAmount):
+        try:
+            checkout_session = stripe.checkout.Session.create(
+                line_items=[
+                    {
+                        'price_data': {
+                            'currency': 'usd',
+                            'product_data': {
+                                'name': 'Your Order',
+                            },
+                            'unit_amount': customAmount,
+                        },
+                        'quantity': 1,
+                    },
+                ],
+                payment_method_types = ['card'],
+                mode='payment',
+                success_url= settings.SITE_URL + '/?success=true&session_id={CHECKOUT_SESSION_ID}',
+                cancel_url= settings.SITE_URL + '/?canceled=true',
+            )
+            return redirect(checkout_session.url)
+        
+        except:
+            return Response(
+                {'error': 'Something went wrong when creating stripe checkout session'},
+                status = status.status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
+
+       
