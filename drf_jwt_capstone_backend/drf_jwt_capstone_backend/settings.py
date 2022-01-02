d7a9145c85d191952e3912eaab36841d1df5c3e2
andrew morales <andrew.morales725@gmail.com>
2022-01-02 15:50:48 -0500

    Changed default site url for post payment redirect

--- drf_jwt_capstone_backend/drf_jwt_capstone_backend/settings.py
+++ drf_jwt_capstone_backend/drf_jwt_capstone_backend/settings.py
@@ -134,7 +134,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework_simplejwt.authentication.JWTAuthentication',)
 }
 
-SITE_URL = 'http://localhost:3000'
+SITE_URL = 'http://localhost:3000/home'
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(days=10),
