3b6696857bfd9c7ed74360ca7778abb304cce25d
andrew morales <andrew.morales725@gmail.com>
2022-01-18 20:33:22 -0500

    Updated orders serializer to accept lunchgroup id in post request

--- drf_jwt_capstone_backend/orders/serializers.py
+++ drf_jwt_capstone_backend/orders/serializers.py
@@ -1,7 +1,9 @@
 from rest_framework import serializers
 from .models import Order
+from lunchgroups.models import LunchGroup
 
 class OrderSerializer(serializers.ModelSerializer):
+    lunchgroup = serializers.PrimaryKeyRelatedField(queryset=LunchGroup.objects.all())
     class Meta:
         model = Order
         fields = ['id', 'lunchgroup', 'user_id', 'order_content', 'price', 'notes', 'status']
